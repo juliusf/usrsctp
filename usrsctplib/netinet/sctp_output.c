af18eaf511163ace81024663af5ed1d57720caba
Julius Flohr <julius.flohr@gmail.com>
2019-11-18 14:02:08 +0000

    update mtu only, if mtu reported by route is smaller than smallest_mtu

--- usrsctplib/netinet/sctp_output.c
+++ usrsctplib/netinet/sctp_output.c
@@ -5016,8 +5016,8 @@ sctp_lowlevel_chunk_output(struct sctp_inpcb *inp,
 						}
 						if ((stcb != NULL) && (stcb->asoc.smallest_mtu > mtu)) {
 							sctp_mtu_size_reset(inp, &stcb->asoc, mtu);
+							net->mtu = mtu;
 						}
-						net->mtu = mtu;
 					} else {
 						if (net->port) {
 							net->mtu -= sizeof(struct udphdr);
